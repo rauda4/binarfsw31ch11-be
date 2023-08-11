13bd8f7b52b2d0fa4bc050df396161b275b73a2f
rafikaauda <rafika.auda4@gmail.com>
2023-08-11 13:17:10 +0700

    change folder temp to public static

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const flash = require("express-flash");
+const express = require('express');
+const flash = require('express-flash');
 const app = express();
-const routes = require("./routes");
+const routes = require('./routes');
 const cors = require('cors');
-const cookieParser = require("cookie-parser");
-const handleCors = require("./middlewares/cors");
+const cookieParser = require('cookie-parser');
+const handleCors = require('./middlewares/cors');
 
-require("dotenv").config();
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("Public"));
+app.use(express.static('temp'));
 
 app.use(flash());
 app.use(cors());
 app.use(cookieParser());
 app.use(handleCors);
 
-app.get("/", (req, res) => {
-  res.json({ message: "api running !!" });
+app.get('/', (req, res) => {
+  res.json({ message: 'api running !!' });
 });
 
-app.use("/", routes);
+app.use('/', routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
