39f9e27a37f6372dc584a0671bae29f0b90533bb
rafikaauda <rafika.auda4@gmail.com>
2023-08-11 15:03:21 +0700

    update cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,14 @@
 const handleCors = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type,token, authorization"
-    ); // intercept OPTIONS method
-    if ("OPTIONS" == req.method) {
-      res.send(200);
-    } else {
-      next();
-    }
-  };
-  module.exports = handleCors;
-  
+  res.header('Access-Control-Allow-Origin', 'https://localhost:3000/');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type,token, authorization'
+  ); // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+module.exports = handleCors;
