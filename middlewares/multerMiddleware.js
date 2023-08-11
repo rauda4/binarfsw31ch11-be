7a0ecba53cee9efe7c6dae296ed9ff3e93df1b2c
rafikaauda <rafika.auda4@gmail.com>
2023-08-11 13:42:08 +0700

    change path

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -1,10 +1,10 @@
 const multer = require('multer');
 const path = require('path');
-const tempDir = path.join(__dirname, '../temp'); // Pastikan "/temp" sesuai dengan direktori penyimpanan sementara yang benar
+// const tempDir = path.join(__dirname, '../temp'); // Pastikan "/temp" sesuai dengan direktori penyimpanan sementara yang benar
 
 const diskStorage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, tempDir);
+    callback(null, path.join(__dirname, +'../temp'));
   },
   filename: (req, file, callback) => {
     callback(null, file.originalname);
